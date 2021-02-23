dface3a3c8819f4f0670349ce1bed6be679723b3
Cameron <yeec327@gmail.com>
2021-02-22 16:59:49 -0800

    Successfully retrieved the message from Slack. Now we can work on translating it and sending it into a thread.

--- week3/app.js
+++ week3/app.js
@@ -19,8 +19,37 @@ app.event('reaction_added', async ({ event, client }) => {
 
     // Now we need the channelID and we could do it like this
     // Get info from the event, like the channelID and timestamp
-    const { item: { channel, ts } } = event;
+    // This is basically all the things we can get from event
+    const { item: { channel, ts }, reaction } = event;
 
+    // now try to get language info from reactions
+    // We will try to do: :flag-mx" as a reaction -> 'es', 'Spanish'
+    // build a function to do it and put the function at the bottom of the code
+    const language = getLanguageFromReaction(reaction);
+    // If they react with an emoji that isn't a flag, then just ignore it.
+    if (!language) return;
+
+    // Then get the text to translate
+    const historyResult = await client.conversations.history({
+      // we already got the channel ID from the top by getting it from events
+      channel: channel,
+      // These 4 are just the paramters so we only get that message and nothing else. Inclusive is referring to between oldest and latest.
+      oldest: ts,
+      latest: ts,
+      inclusive: true,
+      limit: 1,
+    });
+    if (historyResult.messages.length <= 0) return; //because something probably went wrong
+    // here, we are putting the message into a variable called textToTranslate
+    const { text: textToTranslate } = historyResult.messages[0];
+    // see if the message we are getting from Slack is correct
+    console.log(textToTranslate);
+
+
+    // Translate
+
+
+    // send the translated message back to slack
     try {
       // Call chat.postMessage with the built-in client
       const result = await client.chat.postMessage({
@@ -40,3 +69,39 @@ app.event('reaction_added', async ({ event, client }) => {
     await app.start(process.env.PORT || 3000);
     console.log('Bolt app is running');
 })();
+
+
+/**
+ * Get language info from Slack emoji reaction
+ * @param {String} reaction 
+ * @returns {Object} or undefined if unsupported emoji
+ */
+function getLanguageFromReaction(reaction) {
+  // map of emoji codes to language info
+  // Language codes: https://cloud.google.com/translate/docs/languages
+  const reactionToLanguageMap = {
+    // Syntax: First is the Slack's emoji name, and then provide an object that is useful for the Google translate API
+    fr: { code: 'fr', name: 'French' },
+    mx: { code: 'es', name: 'Spanish' },
+    jp: { code: 'ja', name: 'Japnase' },
+  };
+
+  // Get the country codes of the language to translate
+  // Some reactions have :flag-mx: while others are :fr: for some reason
+  // So, we can split and grab the last element
+  // If it has the flag, split. Else, nothing.
+  const [prefix, emojiCode] = reaction.includes('flag-') ? reaction.split('-') : ['', reaction];
+  // We dumped the prefix into the prefix up there and never use it and we dump the useful part into emojiCode.
+  const language = reactionToLanguageMap[emojiCode];
+
+  return language;
+
+}
+
+
+
+
+
+
+
+

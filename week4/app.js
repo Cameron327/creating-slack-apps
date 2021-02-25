98fd2d49e73840efe8e5411c6a62ae67e7ba112d
Cameron <yeec327@gmail.com>
2021-02-24 18:02:12 -0800

    Successfully able to post a message to Slack after getting an event from github using github's webhooks

--- week4/app.js
+++ week4/app.js
@@ -4,20 +4,35 @@ const { config } = require('dotenv');
 
 config();
 
-const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-});
-
 const receiver = new ExpressReceiver({signingSecret: process.env.SLACK_SIGNING_SECRET});
 // This is the middleware to parse the json for req.body
 receiver.router.use(express.json());
 
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    // This needs to be receiver instead of the signingSecret now
+    receiver,
+});
+
 // This is the custom url
 // If that link is hit in our server, it's gonna call the async function
-receiver.router.post('/github-stars', async (req, res) => {
+receiver.router.post('/github-starring', async (req, res) => {
     console.log(req.body);
     console.log('Hit my route for github stars');
+
+    // Now we want to pull info out from the body
+    const { action, repository, sender } = req.body
+    const verb = action === 'deleted' ? 'unstarred' : 'starred';
+    const text = `${sender.login} just ${verb} the ${repository.name} repository, bringing the star count to ${repository.stargazers_count}.`
+
+    app.client.chat.postMessage({
+        token: process.env.SLACK_BOT_TOKEN,
+        channel: 'general', 
+        text,
+    })
+
+
+
     res.sendStatus(200);
 });
 
